89778277a39dd16bf4d65c69c2574e5c87c7f7a8
Cijo Paul <pl.cijo@gmail.com>
2019-04-10 17:57:52 +0530

    pip istall these

--- datacollection/collector/models.py
+++ datacollection/collector/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 class Family(models.Model):
     folder_number = models.CharField(max_length=255)
-    address = models.TextField()
+    address = models.TextField(blank=True)
 
     def __str__(self):
-        return str(self.folder_number)
+        return str(self.folder_number) or ''
 
 
 class Member(models.Model):
@@ -33,32 +33,32 @@ class Member(models.Model):
         ('unknown', 'unknown')
     )
 
-    MARITAL_STATUS = (('married', 'married'), ('unmarried', 'unmarried'),)
-    GENDER_VALUES = (('male', 'male'), ('female', 'female'))
+    MARITAL_STATUS = (('married', 'married'), ('unmarried', 'unmarried'),('unkown','unknown'))
+    GENDER_VALUES = (('male', 'male'), ('female', 'female'), ('other', 'other'))
 
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
     age = models.IntegerField(blank=True)
     gender = models.CharField(choices=GENDER_VALUES, max_length=255, default='female')
     relationship_to_head_of_family = models.CharField(
         choices=HUMAN_RELATIONSHIPS, max_length=255, default='head'
     )
-    education = models.CharField(max_length=255)
-    occupation = models.CharField(max_length=255)
-    income = models.IntegerField()
+    education = models.CharField(max_length=255,blank=True )
+    occupation = models.CharField(max_length=255, blank=True)
+    income = models.IntegerField(blank= True)
     marital_status = models.CharField(max_length=255, choices = MARITAL_STATUS)
-    immunization = models.TextField(max_length=255)
-    remarks = models.TextField()
+    immunization = models.TextField(max_length=255, blank =True)
+    remarks = models.TextField(blank = True)
     family = models.ForeignKey(Family, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.name or ''
 
 
 class MedicalRecord(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     clinical_details = models.TextField()
-    diagnosis = models.TextField()
+    diagnosis = models.TextField(blank=True)
     date = models.DateField()
 
     def __str__(self):
-        return self.member
+        return self.member or ''
